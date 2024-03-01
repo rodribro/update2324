4470e2ab997b1bb0037cb44922b7392cac3663e4
RodrigoCastro <rodrigo.castro.pt@gmail.com>
2024-03-01 01:07:11 +0000

    Revert "fe and be"
    This reverts commit 976ee709bf569c584f43ad2b2f43ed6db0c53a23.

--- src/main/java/ncc/up/pt/updateBackend/Repository/AttendeesRepository.java
+++
@@ -1,9 +0,0 @@
-package ncc.up.pt.updateBackend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ncc.up.pt.updateBackend.Model.Attendees;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendeesRepository extends JpaRepository<Attendees, Long> {
-}
